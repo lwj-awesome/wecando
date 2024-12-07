9003ba2a3bbd81a6eead27687fcf52918902cd24
lwj <lwj1267@naver.com>
2024-12-07 16:47:41 +0900

    ui 패키지에 themse.css import 완료 console.log 확인

--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { ReactNode } from "react";
-
+import { colors } from "@wecando/themes";
 interface ButtonProps {
   children: ReactNode;
   className?: string;
@@ -9,12 +9,15 @@ interface ButtonProps {
 }
 
 export const Button = ({ children, className, appName }: ButtonProps) => {
+  const test = colors.palette.blue[200];
+  console.log(test);
   return (
     <button
       className={className}
       onClick={() => alert(`Hello from your ${appName} app!`)}
     >
       {children}
+      <span style={{ color: test }}>zzzz</span>
     </button>
   );
 };
